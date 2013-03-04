ed2209dfb407347f326850a06d1f5182f33ee8e2
dclambert <dcl@panix.com>
2013-03-04 10:36:56 -0600

    Initial commit

---
+++ elm.py
@@ -0,0 +1,566 @@
+# -*- coding: utf8
+# Author: David C. Lambert [dcl -at- panix -dot- com]
+# Copyright(c) 2013
+# License: Simple BSD
+
+"""
+The :mod:`sklearn.neural_networks.elm` module implements the
+Extreme Learning Machine Classifiers and Regressors (ELMClassifier,
+ELMRegressor, SimpleELMRegressor, SimpleELMClassifier).
+
+An Extreme Learning Machine (ELM) is a single layer feedforward
+network with a random hidden layer components and least-squares fitting
+of the hidden->output weights by default. [1][2]
+
+References
+----------
+.. [1] http://www.extreme-learning-machines.org
+.. [2] G.-B. Huang, Q.-Y. Zhu and C.-K. Siew, "Extreme Learning Machine:
+          Theory and Applications", Neurocomputing, vol. 70, pp. 489-501,
+          2006.
+"""
+
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+from scipy.linalg import pinv2
+
+from sklearn.utils import as_float_array
+from sklearn.utils.extmath import safe_sparse_dot
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
+from sklearn.preprocessing import LabelBinarizer
+
+from random_hidden_layer import SimpleRandomHiddenLayer
+
+__all__ = ["ELMRegressor",
+           "ELMClassifier",
+           "SimpleELMRegressor",
+           "SimpleELMClassifier"]
+
+
+# BaseELM class, regressor and hidden_layer attributes
+# and provides defaults for docstrings
+class BaseELM(BaseEstimator):
+    """
+    Base class for ELMs.
+
+    Warning: This class should not be used directly.
+    Use derived classes instead.
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(self, hidden_layer, regressor):
+        self.regressor = regressor
+        self.hidden_layer = hidden_layer
+
+    @abstractmethod
+    def fit(self, X, y):
+        """
+        Fit the model using X, y as training data.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like of shape [n_samples, n_outputs]
+            Target values (class labels in classification, real numbers in
+            regression)
+
+        Returns
+        -------
+        self : object
+
+            Returns an instance of self.
+        """
+
+    @abstractmethod
+    def predict(self, X):
+        """
+        Predict values using the model
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+
+        Returns
+        -------
+        C : numpy array of shape [n_samples, n_outputs]
+            Predicted values.
+        """
+
+
+class ELMRegressor(BaseELM, RegressorMixin):
+    """
+    ELMRegressor is a regressor based on the Extreme Learning Machine.
+
+    An Extreme Learning Machine (ELM) is a single layer feedforward
+    network with a random hidden layer components and least-squares fitting
+    of the hidden->output weights by default. [1][2]
+
+    Parameters
+    ----------
+    `hidden_layer` : random_hidden_layer instance, optional
+        (default=SimpleRandomHiddenLayer(random_state=0))
+
+    `regressor`    : linear_model instance, optional (default=None)
+        If provided, this object is used to perform the regression from hidden
+        unit activations to the outputs and subsequent predictions.  If not
+        present, a simple least squares fit is performed internally.
+
+    Attributes
+    ----------
+    `coefs_` : numpy array
+        Fitted regression coefficients if no regressor supplied.
+
+    `fitted_` : bool
+        Flag set when fit has been called already.
+
+    `hidden_activations_` : numpy array of shape [n_samples, n_hidden]
+        Hidden layer activations for last input.
+
+    See Also
+    --------
+    RBFRandomHiddenLayer, SimpleRandomHiddenLayer, ELMRegressor, ELMClassifier
+    SimpleELMRegressor, SimpleELMClassifier
+
+    References
+    ----------
+    .. [1] http://www.extreme-learning-machines.org
+    .. [2] G.-B. Huang, Q.-Y. Zhu and C.-K. Siew, "Extreme Learning Machine:
+          Theory and Applications", Neurocomputing, vol. 70, pp. 489-501,
+              2006.
+    """
+
+    def __init__(self,
+                 hidden_layer=SimpleRandomHiddenLayer(random_state=0),
+                 regressor=None):
+
+        super(ELMRegressor, self).__init__(hidden_layer, regressor)
+
+        self.coefs_ = None
+        self.fitted_ = False
+        self.hidden_activations_ = None
+
+    def _fit_regression(self, y):
+        """fit regression using internal least squares/supplied regressor"""
+        if (self.regressor is None):
+            self.coefs_ = safe_sparse_dot(pinv2(self.hidden_activations_), y)
+        else:
+            self.regressor.fit(self.hidden_activations_, y)
+
+        self.fitted_ = True
+
+    def fit(self, X, y):
+        """
+        Fit the model using X, y as training data.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like of shape [n_samples, n_outputs]
+            Target values (class labels in classification, real numbers in
+            regression)
+
+        Returns
+        -------
+        self : object
+
+            Returns an instance of self.
+        """
+        # fit random hidden layer and compute the hidden layer activations
+        self.hidden_activations_ = self.hidden_layer.fit_transform(X)
+
+        # solve the regression from hidden activations to outputs
+        self._fit_regression(as_float_array(y, copy=True))
+
+        return self
+
+    def _get_predictions(self, X):
+        """get predictions using internal least squares/supplied regressor"""
+        if (self.regressor is None):
+            preds = safe_sparse_dot(self.hidden_activations_, self.coefs_)
+        else:
+            preds = self.regressor.predict(self.hidden_activations_)
+
+        return preds
+
+    def predict(self, X):
+        """
+        Predict values using the model
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+
+        Returns
+        -------
+        C : numpy array of shape [n_samples, n_outputs]
+            Predicted values.
+        """
+        if (not self.fitted_):
+            raise ValueError("ELMRegressor not fitted")
+
+        # compute hidden layer activations
+        self.hidden_activations_ = self.hidden_layer.transform(X)
+
+        # compute output predictions for new hidden activations
+        predictions = self._get_predictions(X)
+
+        return predictions
+
+
+class ELMClassifier(BaseELM, ClassifierMixin):
+    """
+    ELMClassifier is a classifier based on the Extreme Learning Machine.
+
+    An Extreme Learning Machine (ELM) is a single layer feedforward
+    network with a random hidden layer components and least-squares fitting
+    of the hidden->output weights by default. [1][2]
+
+    Parameters
+    ----------
+    `hidden_layer` : random_hidden_layer instance, optional
+        (default=SimpleRandomHiddenLayer(random_state=0))
+
+    `regressor`    : linear_model instance, optional (default=None)
+        If provided, this object is used to perform the regression from hidden
+        unit activations to the outputs and subsequent predictions.  If not
+        present, a simple least squares fit is performed internally.
+
+    Attributes
+    ----------
+    `classes_` : numpy array of shape [n_classes]
+        Array of class labels
+
+    `binarizer_` : LabelBinarizer instance
+        Used to transform class labels
+
+    `elm_regressor_` : ELMRegressor instance
+        Performs actual fit of binarized values
+
+    See Also
+    --------
+    RBFRandomHiddenLayer, SimpleRandomHiddenLayer, ELMRegressor, ELMClassifier
+    SimpleELMRegressor, SimpleELMClassifier
+
+    References
+    ----------
+    .. [1] http://www.extreme-learning-machines.org
+    .. [2] G.-B. Huang, Q.-Y. Zhu and C.-K. Siew, "Extreme Learning Machine:
+              Theory and Applications", Neurocomputing, vol. 70, pp. 489-501,
+              2006.
+    """
+    def __init__(self,
+                 hidden_layer=SimpleRandomHiddenLayer(random_state=0),
+                 regressor=None):
+
+        super(ELMClassifier, self).__init__(hidden_layer, regressor)
+
+        self.classes_ = None
+        self.binarizer_ = LabelBinarizer(-1, 1)
+        self.elm_regressor_ = ELMRegressor(hidden_layer, regressor)
+
+    def decision_function(self, X):
+        """
+        This function return the decision function values related to each
+        class on an array of test vectors X.
+
+        Parameters
+        ----------
+        X : array-like of shape [n_samples, n_features]
+
+        Returns
+        -------
+        C : array of shape [n_samples, n_classes] or [n_samples,]
+            Decision function values related to each class, per sample.
+            In the two-class case, the shape is [n_samples,]
+        """
+        return self.elm_regressor_.predict(X)
+
+    def fit(self, X, y):
+        """
+        Fit the model using X, y as training data.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like of shape [n_samples, n_outputs]
+            Target values (class labels in classification, real numbers in
+            regression)
+
+        Returns
+        -------
+        self : object
+
+            Returns an instance of self.
+        """
+        self.classes_ = np.unique(y)
+
+        y_bin = self.binarizer_.fit_transform(y)
+
+        self.elm_regressor_.fit(X, y_bin)
+        return self
+
+    def predict(self, X):
+        """Predict values using the model
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+
+        Returns
+        -------
+        C : numpy array of shape [n_samples, n_outputs]
+            Predicted values.
+        """
+        raw_predictions = self.decision_function(X)
+        class_predictions = self.binarizer_.inverse_transform(raw_predictions)
+
+        return class_predictions
+
+
+# ELMRegressor with default SimpleRandomHiddenLayer
+class SimpleELMRegressor(BaseEstimator, RegressorMixin):
+    """
+    SimpleELMRegressor is a regressor based on the Extreme Learning Machine.
+
+    An Extreme Learning Machine (ELM) is a single layer feedforward
+    network with a random hidden layer components and least-squares fitting
+    of the hidden->output weights by default. [1][2]
+
+    SimpleELMRegressor is a wrapper for an ELMRegressor that uses a
+    SimpleRandomHiddenLayer and passes the __init__ parameters through
+    to the hidden layer generated by the fit() method.
+
+    Parameters
+    ----------
+    `n_hidden` : int, optional (default=20)
+        Number of units to generate in the SimpleRandomHiddenLayer
+
+    `xfer_func` : {callable, string} optional (default='tanh')
+        Function used to transform input activation
+        It must be one of 'tanh', 'sine', 'tribas', 'sigmoid', 'hardlim' or
+        a callable.  If none is given, 'tanh' will be used. If a callable
+        is given, it will be used to compute the hidden unit activations.
+
+    `xfer_args` : dictionary, optional (default=None)
+        Supplies keyword arguments for a callable xfer_func
+
+    `random_state`  : int, RandomState instance or None (default=None)
+        Control the pseudo random number generator used to generate the
+        hidden unit weights at fit time.
+
+    Attributes
+    ----------
+    `elm_regressor_` : ELMRegressor object
+        Wrapped object that actually performs the fit.
+
+    See Also
+    --------
+    RBFRandomHiddenLayer, SimpleRandomHiddenLayer, ELMRegressor, ELMClassifier
+    SimpleELMRegressor, SimpleELMClassifier
+
+    References
+    ----------
+    .. [1] http://www.extreme-learning-machines.org
+    .. [2] G.-B. Huang, Q.-Y. Zhu and C.-K. Siew, "Extreme Learning Machine:
+          Theory and Applications", Neurocomputing, vol. 70, pp. 489-501,
+              2006.
+    """
+
+    def __init__(self, n_hidden=20, xfer_func='tanh', xfer_args=None,
+                 random_state=None):
+
+        self.n_hidden = n_hidden
+        self.xfer_func = xfer_func
+        self.xfer_args = xfer_args
+        self.random_state = random_state
+
+        self.elm_regressor_ = None
+
+    def fit(self, X, y):
+        """
+        Fit the model using X, y as training data.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like of shape [n_samples, n_outputs]
+            Target values (class labels in classification, real numbers in
+            regression)
+
+        Returns
+        -------
+        self : object
+
+            Returns an instance of self.
+        """
+        rhl = SimpleRandomHiddenLayer(n_hidden=self.n_hidden,
+                                      xfer_func=self.xfer_func,
+                                      xfer_args=self.xfer_args,
+                                      random_state=self.random_state)
+
+        self.elm_regressor_ = ELMRegressor(hidden_layer=rhl)
+        self.elm_regressor_.fit(X, y)
+        return self
+
+    def predict(self, X):
+        """
+        Predict values using the model
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+
+        Returns
+        -------
+        C : numpy array of shape [n_samples, n_outputs]
+            Predicted values.
+        """
+        if (self.elm_regressor_ is None):
+            raise ValueError("SimpleELMRegressor not fitted")
+
+        return self.elm_regressor_.predict(X)
+
+
+# ELMClassifier with default SimpleRandomHiddenLayer
+class SimpleELMClassifier(BaseEstimator, ClassifierMixin):
+    """
+    SimpleELMClassifier is a classifier based on the Extreme Learning Machine.
+
+    An Extreme Learning Machine (ELM) is a single layer feedforward
+    network with a random hidden layer components and least-squares fitting
+    of the hidden->output weights by default. [1][2]
+
+    SimpleELMClassifier is a wrapper for an ELMClassifier that uses a
+    SimpleRandomHiddenLayer and passes the __init__ parameters through
+    to the hidden layer generated by the fit() method.
+
+    Parameters
+    ----------
+    `n_hidden` : int, optional (default=20)
+        Number of units to generate in the SimpleRandomHiddenLayer
+
+    `xfer_func` : {callable, string} optional (default='tanh')
+        Function used to transform input activation
+        It must be one of 'tanh', 'sine', 'tribas', 'sigmoid', 'hardlim' or
+        a callable.  If none is given, 'tanh' will be used. If a callable
+        is given, it will be used to compute the hidden unit activations.
+
+    `xfer_args` : dictionary, optional (default=None)
+        Supplies keyword arguments for a callable xfer_func
+
+    `random_state`  : int, RandomState instance or None (default=None)
+        Control the pseudo random number generator used to generate the
+        hidden unit weights at fit time.
+
+    Attributes
+    ----------
+    `classes_` : numpy array of shape [n_classes]
+        Array of class labels
+
+    `elm_classifier_` : ELMClassifier object
+        Wrapped object that actually performs the fit
+
+    See Also
+    --------
+    RBFRandomHiddenLayer, SimpleRandomHiddenLayer, ELMRegressor, ELMClassifier
+    SimpleELMRegressor, SimpleELMClassifier
+
+    References
+    ----------
+    .. [1] http://www.extreme-learning-machines.org
+    .. [2] G.-B. Huang, Q.-Y. Zhu and C.-K. Siew, "Extreme Learning Machine:
+          Theory and Applications", Neurocomputing, vol. 70, pp. 489-501,
+              2006.
+    """
+
+    def __init__(self, n_hidden=20, xfer_func='tanh', xfer_args=None,
+                 random_state=None):
+
+        self.n_hidden = n_hidden
+        self.xfer_func = xfer_func
+        self.xfer_args = xfer_args
+        self.random_state = random_state
+
+        self.elm_classifier_ = None
+
+    @property
+    def classes_(self):
+        return self.elm_classifier_.classes_
+
+    def decision_function(self, X):
+        """
+        This function return the decision function values related to each
+        class on an array of test vectors X.
+
+        Parameters
+        ----------
+        X : array-like of shape [n_samples, n_features]
+
+        Returns
+        -------
+        C : array of shape [n_samples, n_classes] or [n_samples,]
+            Decision function values related to each class, per sample.
+            In the two-class case, the shape is [n_samples,]
+        """
+        return self.elm_classifier_.decision_function(X)
+
+    def fit(self, X, y):
+        """
+        Fit the model using X, y as training data.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like of shape [n_samples, n_outputs]
+            Target values (class labels in classification, real numbers in
+            regression)
+
+        Returns
+        -------
+        self : object
+
+            Returns an instance of self.
+        """
+        rhl = SimpleRandomHiddenLayer(n_hidden=self.n_hidden,
+                                      xfer_func=self.xfer_func,
+                                      xfer_args=self.xfer_args,
+                                      random_state=self.random_state)
+
+        self.elm_classifier_ = ELMClassifier(hidden_layer=rhl)
+        self.elm_classifier_.fit(X, y)
+
+        return self
+
+    def predict(self, X):
+        """
+        Predict values using the model
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape [n_samples, n_features]
+
+        Returns
+        -------
+        C : numpy array of shape [n_samples, n_outputs]
+            Predicted values.
+        """
+        if (self.elm_classifier_ is None):
+            raise ValueError("SimpleELMClassifier not fitted")
+
+        return self.elm_classifier_.predict(X)
---
+++ elm_notebook.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+# <nbformat>2</nbformat>
+
+# <codecell>
+
+# Demo python notebook for sklearn elm and random_hidden_layer modules
+#
+# Author: David C. Lambert
+# License: Simple BSD
+
+# <codecell>
+
+from time import time
+from sklearn.cluster import k_means
+from elm import ELMClassifier, ELMRegressor, SimpleELMClassifier, SimpleELMRegressor
+from random_hidden_layer import SimpleRandomHiddenLayer, RBFRandomHiddenLayer
+
+# <codecell>
+
+def make_toy():
+    x = np.arange(0.25,20,0.1)
+    y = x*np.cos(x)+np.random.randn(x.shape[0])
+    x = x.reshape(-1,1)
+    y = y.reshape(-1,1)
+    return x, y
+
+# <codecell>
+
+def res_dist(x, y, e, n_runs=100, random_state=None):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=random_state)
+
+    test_res = []
+    train_res = []
+    start_time = time()
+
+    for i in xrange(n_runs):
+        e.fit(x_train, y_train)
+        train_res.append(e.score(x_train, y_train))
+        test_res.append(e.score(x_test, y_test))
+        if (i%5 == 0): print "%d"%i,
+
+    print "\nTime: %.3f secs" % (time() - start_time)
+
+    print "Test Min: %.3f Mean: %.3f Max: %.3f SD: %.3f" % (min(test_res), mean(test_res), max(test_res), std(test_res))
+    print "Train Min: %.3f Mean: %.3f Max: %.3f SD: %.3f" % (min(train_res), mean(train_res), max(train_res), std(train_res))
+    print
+    return (train_res, test_res)
+
+# <codecell>
+
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_iris, load_digits, load_diabetes, make_regression
+
+stdsc = StandardScaler()
+
+iris = load_iris()
+irx, iry = stdsc.fit_transform(iris.data), iris.target
+irx_train, irx_test, iry_train, iry_test = train_test_split(irx, iry, test_size=0.2)
+
+digits = load_digits()
+dgx, dgy = stdsc.fit_transform(digits.data), digits.target
+dgx_train, dgx_test, dgy_train, dgy_test = train_test_split(dgx, dgy, test_size=0.2)
+
+diabetes = load_diabetes()
+#dbx, dby = stdsc.fit_transform(diabetes.data), stdsc.fit_transform(diabetes.target)
+dbx, dby = stdsc.fit_transform(diabetes.data), diabetes.target
+dbx_train, dbx_test, dby_train, dby_test = train_test_split(dbx, dby, test_size=0.2)
+
+mrx, mry = make_regression(n_samples=2000)
+mrx_train, mrx_test, mry_train, mry_test = train_test_split(mrx, mry, test_size=0.2)
+
+xtoy, ytoy = make_toy()
+xtoy, ytoy = stdsc.fit_transform(xtoy), stdsc.fit_transform(ytoy)
+xtoy_train, xtoy_test, ytoy_train, ytoy_test = train_test_split(xtoy, ytoy, test_size=0.2)
+plot(xtoy, ytoy)
+
+# <codecell>
+
+# RBF tests
+elmc = ELMClassifier(RBFRandomHiddenLayer(xfer_func='gaussian'))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+elmc = ELMClassifier(RBFRandomHiddenLayer(xfer_func='poly_spline', gamma=2))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+elmc = ELMClassifier(RBFRandomHiddenLayer(xfer_func='multiquadric'))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+# Simple tests
+elmc = ELMClassifier(SimpleRandomHiddenLayer(xfer_func='sine'))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+elmc = ELMClassifier(SimpleRandomHiddenLayer(xfer_func='tanh'))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+elmc = ELMClassifier(SimpleRandomHiddenLayer(xfer_func='tribas'))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+elmc = ELMClassifier(SimpleRandomHiddenLayer(xfer_func='sigmoid'))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+elmc = ELMClassifier(SimpleRandomHiddenLayer(xfer_func='hardlim'))
+tr,ts = res_dist(irx, iry, elmc, n_runs=100, random_state=0)
+
+# <codecell>
+
+hardlim = (lambda a: np.array(a > 0.0, dtype=float))
+tribas = (lambda a: np.clip(1.0 - np.fabs(a), 0.0, 1.0))
+elmr = ELMRegressor(SimpleRandomHiddenLayer(random_state=0, xfer_func=tribas))
+elmr.fit(xtoy_train, ytoy_train)
+print elmr.score(xtoy_train, ytoy_train), elmr.score(xtoy_test, ytoy_test)
+plot(xtoy, ytoy, xtoy, elmr.predict(xtoy))
+
+# <codecell>
+
+rhl = SimpleRandomHiddenLayer(n_hidden=200)
+elmr = ELMRegressor(hidden_layer=rhl)
+tr, ts = res_dist(mrx, mry, elmr, n_runs=20, random_state=0)
+
+# <codecell>
+
+rhl = RBFRandomHiddenLayer(n_hidden=15, gamma=0.25)
+elmr = ELMRegressor(hidden_layer=rhl)
+elmr.fit(xtoy_train, ytoy_train)
+print elmr.score(xtoy_train, ytoy_train), elmr.score(xtoy_test, ytoy_test)
+plot(xtoy, ytoy, xtoy, elmr.predict(xtoy))
+
+# <codecell>
+
+nh = 10
+(ctrs, _, _) = k_means(xtoy_train, nh)
+unit_rs = np.ones(nh)
+rhl = RBFRandomHiddenLayer(n_hidden=nh, xfer_func='poly_spline', gamma=5)
+#rhl = RBFRandomHiddenLayer(n_hidden=nh, xfer_func='multiquadric', gamma=1)
+#rhl = RBFRandomHiddenLayer(n_hidden=nh, centers=ctrs, radii=unit_rs, gamma=4)
+elmr = ELMRegressor(hidden_layer=rhl)
+elmr.fit(xtoy_train, ytoy_train)
+print elmr.score(xtoy_train, ytoy_train), elmr.score(xtoy_test, ytoy_test)
+plot(xtoy, ytoy, xtoy, elmr.predict(xtoy))
+
+# <codecell>
+
+rbf_rhl = RBFRandomHiddenLayer(n_hidden=100, random_state=0, gamma=0.1)
+elmc_rbf = ELMClassifier(hidden_layer=rbf_rhl)
+elmc_rbf.fit(dgx_train, dgy_train)
+print elmc_rbf.score(dgx_train, dgy_train), elmc_rbf.score(dgx_test, dgy_test)
+
+def powtanh_xfer(activations, power=1.0):
+    return pow(np.tanh(activations), power)
+
+#tanh_rhl = SimpleRandomHiddenLayer(n_hidden=5000, random_state=0)
+tanh_rhl = SimpleRandomHiddenLayer(n_hidden=5000, xfer_func=powtanh_xfer, xfer_args={'power':2.0})
+elmc_tanh = ELMClassifier(hidden_layer=tanh_rhl)
+elmc_tanh.fit(dgx_train, dgy_train)
+print elmc_tanh.score(dgx_train, dgy_train), elmc_tanh.score(dgx_test, dgy_test)
+
+# <codecell>
+
+rbf_rhl = RBFRandomHiddenLayer(n_hidden=100, gamma=0.1)
+tr, ts = res_dist(dgx, dgy, ELMClassifier(hidden_layer=rbf_rhl), n_runs=100, random_state=0)
+
+# <codecell>
+
+hist(ts), hist(tr)
+print
+
+# <codecell>
+
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+tr, ts = res_dist(dbx, dby, RandomForestRegressor(n_estimators=15), n_runs=100, random_state=0)
+hist(tr), hist(ts)
+print
+
+rhl = RBFRandomHiddenLayer(n_hidden=15, gamma=0.01)
+tr,ts = res_dist(dbx, dby, ELMRegressor(rhl), n_runs=100, random_state=0)
+hist(tr), hist(ts)
+print
+
+# <codecell>
+
+hist(ts), hist(tr)
+print
+
+# <codecell>
+
+elmc = SimpleELMClassifier(n_hidden=500)
+elmc.fit(dgx_train, dgy_train)
+print elmc.score(dgx_train, dgy_train), elmc.score(dgx_test, dgy_test)
+
+# <codecell>
+
+elmc = SimpleELMClassifier(n_hidden=500, xfer_func='hardlim')
+elmc.fit(dgx_train, dgy_train)
+print elmc.score(dgx_train, dgy_train), elmc.score(dgx_test, dgy_test)
+
+# <codecell>
+
+elmr = SimpleELMRegressor()
+elmr.fit(xtoy_train, ytoy_train)
+print elmr.score(xtoy_train, ytoy_train), elmr.score(xtoy_test, ytoy_test)
+plot(xtoy, ytoy, xtoy, elmr.predict(xtoy))
+
+# <codecell>
+
+elmr = SimpleELMRegressor(xfer_func='tribas')
+elmr.fit(xtoy_train, ytoy_train)
+print elmr.score(xtoy_train, ytoy_train), elmr.score(xtoy_test, ytoy_test)
+plot(xtoy, ytoy, xtoy, elmr.predict(xtoy))
+
